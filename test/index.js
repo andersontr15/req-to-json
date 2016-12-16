bb52733ffe5bfbfaa79115c864b16a3ba2198e04
Theo <theoanderson3@gmail.com>
2016-12-16 17:20:46 -0500

    error trapping

--- test/index.js
+++ test/index.js
@@ -1,26 +1,26 @@
-var express = require('express');
-var jsonStore = require('../index');
-var app = express();
+// var express = require('express');
+// var jsonStore = require('../index');
+// var app = express();
 
-app.get('/', function(request, response) {
-    jsonStore.store('https://api.github.com/users/andersontr15', 'user.json', function(err, data) {
-        if(err) {
-            return response.status(400).send(err);
-        }
-        return response.status(200).send(data);
-    });
-});
+// app.get('/', function(request, response) {
+//     jsonStore.store('https://api.github.com/users/andersontr15', 'user.json', function(err, data) {
+//         if(err) {
+//             return response.status(400).send(err);
+//         }
+//         return response.status(200).send(data);
+//     });
+// });
 
-app.get('/getData', function(request, response) {
-    jsonStore.getJSON('user.json')
-             .then(function(data) {
-                 return response.status(200).send(data);
-             })
-             .catch(function(error) {
-                 return response.status(400).send(error);
-             });
-});
+// app.get('/getData', function(request, response) {
+//     jsonStore.getJSON('user.json')
+//              .then(function(data) {
+//                  return response.status(200).send(data);
+//              })
+//              .catch(function(error) {
+//                  return response.status(400).send(error);
+//              });
+// });
 
-app.listen(8080, function() {
-    console.log('Listening on port 8080');
-});
+// app.listen(8080, function() {
+//     console.log('Listening on port 8080');
+// });
